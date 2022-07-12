e653d79286db95adda5c8c6ecc23736a4a6297e3
zhang.kaiyu <zhang.kaiyu@immomo.com>
2022-07-12 11:00:27 +0800

    fix

---
+++ .leetcode/1.两数之和.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=javascript
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+  const map = new Map();
+  for(let i = 0, len = nums.length;i < len;i++) {
+      if(map.has(target - nums[i])) {
+          return [map.get(target - nums[i]), i];
+      }
+      map.set(nums[i], i);
+  }
+  return [];
+};
+// @lc code=end
+
